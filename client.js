8e814a3dafc7d2e05c66259d03aba5c7618a6b0b
EnvPlayz <82762496+EnvPlayz@users.noreply.github.com>
2021-11-05 18:52:45 +0300

    Delete client.js

--- client.js
+++
@@ -1,8 +0,0 @@
-const socket = io();
-
-socket.on('getcubes', item => {
-    var cubes = item;
-});
-document.getElementById("start").addEventListener("click",function(){
-    socket.emit("start",`User ${socket.id} has joined!`)
-});
